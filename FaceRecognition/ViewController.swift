81cd2611029362b6d85eb183731e183721c64909
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-05 18:42:54 +0300

    Added LocalAuthentication for FaceRecognition

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    // FaceRecognize yapabilmek için info.plist'ten ayar eklemek gerekli (Privacy -FaceID)
+    @IBAction func signInClicked(_ sender: Any) {
+        let  authContext = LAContext()
+        
+        // canEvaluatePolicy bir NSError beklediği için oluşturdum.
+        var error : NSError?
+        
+        // Eğer telefon özellik olarak Yüz tanımaya sahipse... Biometric -> Yüz ya da parmak izi
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            // localized Reason : Neden bu işlemi yapıyorsun ?
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success == true{
+                    // Dispatch kullandım.Çünkü eğer arkaplanda bir işlem yapıyorsak, yani UI'da kullanıcının etkileşimini etkileyecek bir işlem yapıyorsam, bu işlemleri artık farkı bir seviyede, farklı bir thread'de yapmam gerekiyor.Bu yüzden performSegue ve myLabel'mı main thread'te kullanmam gerekiyor.
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                }else{
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error"
+                    }
+                }
+            }
+        }
+    }
 }
 
